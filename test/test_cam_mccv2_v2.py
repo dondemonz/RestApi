d84efc1ae99b9d7b409ec39992321114c0c0ec6f
root <kykareky05@mail.ru>
2019-08-13 15:07:41 +0300

    24

--- test/test_cam_mccv2_v2.py
+++ test/test_cam_mccv2_v2.py
@@ -4,6 +4,7 @@ from model.input_data import *
 
 
 # Запрос на получение настроек всех объектов CAM
+# Не забывать, что у пользователя должны быть проставлены права на УС
 def test_GetV2AllCamerasCode200():
     data = "success"
     # "%23" заменяет символ # для удаленных систем
@@ -15,3 +16,18 @@ def test_GetV2AllCamerasCode200():
     n = data1["status"]
     assert data == n
 
+def test_GetV2AllCamerasCode401():
+    response = requests.get(url="http://"+slave_ip+":"+restPort+"/api/v2/cameras/1%231")
+    user_resp_code = "401"
+    assert str(response.status_code) == user_resp_code
+
+def test_GetV2AllCamerasCode404():
+    data = "Unknown CAM id:1#999"
+    # "%23" заменяет символ # для удаленных систем
+    response = requests.get(url="http://"+slave_ip+":"+restPort+"/api/v2/cameras/1%23999", auth=auth)
+    user_resp_code = "404"
+    assert str(response.status_code) == user_resp_code
+    body = json.dumps(response.json())
+    data1 = json.loads(body)
+    n = data1["message"]
+    assert data == n
