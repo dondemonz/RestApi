2a4d1d32bd6d73d7529a88b53608d23cd6ec5466
root <kykareky05@mail.ru>
2019-12-30 11:05:42 +0300

    24

--- test/test_cam_get_cam_image_v2.py
+++ test/test_cam_get_cam_image_v2.py
@@ -199,7 +199,7 @@ def test_GetV2CamImageCode503():
     archtime = m.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
     response = requests.get(url="http://" + slave_ip + ":"+restPort+"/api/v2/cameras/"+camId+"/image/" + archtime, auth=auth)
     user_resp_code = "503"
-    time.sleep(4)
+    time.sleep(3)
     assert str(response.status_code) == user_resp_code
 
 
